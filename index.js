be390da2e794c51ec121ab659b73e678f2378dff
mblahnik <mblahnik@uwm.edu>
2020-02-02 14:34:30 -0600

    Create App

---
+++ index.js
@@ -0,0 +1,32 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+
+// Listen for app to be ready
+
+app.on('ready', function() {
+  //find().then(devices => console.log(devices));
+
+  // Create new window
+  mainWindow = new BrowserWindow({
+    webPreferences: { nodeIntegration: true }
+  });
+  // Load html into window
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    })
+  );
+  // Quit app when closed
+  mainWindow.on('closed', function() {
+    app.quit();
+  });
+
+  mainWindow.setMenu(null);
+});
